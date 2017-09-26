7f53f80f60338487058828e3a196bc70676ae24b
elPulpo495 <pestala495@gmail.com>
2017-09-26 09:40:38 -0600

    uploaded more configs

---
+++ cmus/lib.pl
@@ -0,0 +1,414 @@
+/home/paule/Music/The Kill.mp3
+/home/paule/Music/This Is War.mp3
+/home/paule/Music/Kryptonite.mp3
+/home/paule/Music/Back In Black.mp3
+/home/paule/Music/Highway to Hell.mp3
+/home/paule/Music/Thunderstruck.mp3
+/home/paule/Music/Lost Stars.mp3
+/home/paule/Music/Bad Vibrations.mp3
+/home/paule/Music/Its Complicated.mp3
+/home/paule/Music/Paranoia.mp3
+/home/paule/Music/Crazy.mp3
+/home/paule/Music/I Don't Want to Miss a Thing.mp3
+/home/paule/Music/Janie's Got A Gun.mp3
+/home/paule/Music/Right Now (Na Na Na).mp3
+/home/paule/Music/Smack That ft. Eminem.mp3
+/home/paule/Music/Sorry, Blame It On Me.mp3
+/home/paule/Music/Anda y Ve (Remix).mp3
+/home/paule/Music/A Love Like War.mp3
+/home/paule/Music/Dear Maria, Count Me In.mp3
+/home/paule/Music/Weightless.mp3
+/home/paule/Music/Breezeblocks.mp3
+/home/paule/Music/Left Hand Free.mp3
+/home/paule/Music/Dangerous Woman .mp3
+/home/paule/Music/Side To Side ft. Nicki Minaj.mp3
+/home/paule/Music/Arabella .mp3
+/home/paule/Music/Do I Wanna Know.mp3
+/home/paule/Music/Fluorescent Adolescent.mp3
+/home/paule/Music/Love Is A Laserquest (Acoustic).mp3
+/home/paule/Music/Mardy Bum.mp3
+/home/paule/Music/Dopamine ft. Cozz.mp3
+/home/paule/Music/Night Job ft. J. Cole.mp3
+/home/paule/Music/Penthouse.mp3
+/home/paule/Music/Loser.mp3
+/home/paule/Music/I Dont Fuck With You.mp3
+/home/paule/Music/I Know.mp3
+/home/paule/Music/Adam's Song.mp3
+/home/paule/Music/Always.mp3
+/home/paule/Music/Bored To Death.mp3
+/home/paule/Music/California .mp3
+/home/paule/Music/Dammit.mp3
+/home/paule/Music/Down.mp3
+/home/paule/Music/First Date.mp3
+/home/paule/Music/Home Is Such A Lonely Place.mp3
+/home/paule/Music/I Miss You.mp3
+/home/paule/Music/Kings of the Weekend.mp3
+/home/paule/Music/Left Alone.mp3
+/home/paule/Music/No Future.mp3
+/home/paule/Music/Teenage Satellites.mp3
+/home/paule/Music/The Party Song.mp3
+/home/paule/Music/Both Of Us.mp3
+/home/paule/Music/Bed Of Roses.mp3
+/home/paule/Music/Don't Wanna Know (Cover).mp3
+/home/paule/Music/Alligator Blood.mp3
+/home/paule/Music/Blasphemy.mp3
+/home/paule/Music/Can You Feel My Heart.mp3
+/home/paule/Music/Doomed.mp3
+/home/paule/Music/Go To Hell For Heavens Sake.mp3
+/home/paule/Music/Happy Song.mp3
+/home/paule/Music/Sleepwalking.mp3
+/home/paule/Music/True Friends.mp3
+/home/paule/Music/De Este Lado Del Camino.mp3
+/home/paule/Music/Eres.mp3
+/home/paule/Music/Ain't No Rest For The Wicked.mp3
+/home/paule/Music/Cigarette Daydreams.mp3
+/home/paule/Music/Come A Little Closer.mp3
+/home/paule/Music/Te Lo Pido Por Favor.mp3
+/home/paule/Music/Chupetazos.mp3
+/home/paule/Music/Optimista.mp3
+/home/paule/Music/Palmar.mp3
+/home/paule/Music/La Planta.mp3
+/home/paule/Music/Eres Tu.mp3
+/home/paule/Music/Hasta La Piel.mp3
+/home/paule/Music/Bombos Y Tarolas.mp3
+/home/paule/Music/Con El Coco Rapado.mp3
+/home/paule/Music/Cosas De La Vida.mp3
+/home/paule/Music/Culon Culito.mp3
+/home/paule/Music/Doctor Marihuana.mp3
+/home/paule/Music/El Arte Del Engaño.mp3
+/home/paule/Music/El Raton y El Queso.mp3
+/home/paule/Music/La Pelotona.mp3
+/home/paule/Music/Leve.mp3
+/home/paule/Music/Los Mensajes del Whatsapp.mp3
+/home/paule/Music/Para Cada Loco.mp3
+/home/paule/Music/Perros.mp3
+/home/paule/Music/Si Te Vienen A Contar.mp3
+/home/paule/Music/Somos Chidos.mp3
+/home/paule/Music/Soy Quien Soy.mp3
+/home/paule/Music/Suena Mamalona.mp3
+/home/paule/Music/Vato Sencillo.mp3
+/home/paule/Music/Do You Feel It.mp3
+/home/paule/Music/Attention.mp3
+/home/paule/Music/In Your Arms.mp3
+/home/paule/Music/Querer.mp3
+/home/paule/Music/Drop The Game.mp3
+/home/paule/Music/Gold.mp3
+/home/paule/Music/Heartbeat.mp3
+/home/paule/Music/A Thousand Years.mp3
+/home/paule/Music/Human.mp3
+/home/paule/Music/Affection.mp3
+/home/paule/Music/Fix You.mp3
+/home/paule/Music/Magic.mp3
+/home/paule/Music/The Scientist.mp3
+/home/paule/Music/Viva La Vida.mp3
+/home/paule/Music/Bloodline.mp3
+/home/paule/Music/Perdoname.mp3
+/home/paule/Music/Lirica Onirica.mp3
+/home/paule/Music/Me Voy A Poner Bien Loco.mp3
+/home/paule/Music/Serenata Rap.mp3
+/home/paule/Music/Coming In Hot.mp3
+/home/paule/Music/Middle.mp3
+/home/paule/Music/La Recompensa.mp3
+/home/paule/Music/Energy.mp3
+/home/paule/Music/Headlines (Explicit).mp3
+/home/paule/Music/Marvins Room.mp3
+/home/paule/Music/Hoja en Blanco.mp3
+/home/paule/Music/Que Hago Yo.mp3
+/home/paule/Music/Tu Sin Mi.mp3
+/home/paule/Music/Between The Bars.mp3
+/home/paule/Music/Las Piedras Rodantes.mp3
+/home/paule/Music/Parece Facil.mp3
+/home/paule/Music/Beautiful.mp3
+/home/paule/Music/Beautiful Pain.mp3
+/home/paule/Music/Kings Never Die.mp3
+/home/paule/Music/Lose Yourself.mp3
+/home/paule/Music/Eminem.mp3
+/home/paule/Music/Space Bound.mp3
+/home/paule/Music/Till I Collapse.mp3
+/home/paule/Music/Sábado Perpetuo.mp3
+/home/paule/Music/Lamento Boliviano.mp3
+/home/paule/Music/Luz de Dia.mp3
+/home/paule/Music/Tu Carcel.mp3
+/home/paule/Music/Somos Ajenos.mp3
+/home/paule/Music/Broken Frames.mp3
+/home/paule/Music/Darling.mp3
+/home/paule/Music/Game Over.mp3
+/home/paule/Music/Im Not A Vampire.mp3
+/home/paule/Music/Superhero.mp3
+/home/paule/Music/The Drug In Me Is You.mp3
+/home/paule/Music/One In The Chamber & The End Of The Beginning.mp3
+/home/paule/Music/The Show Must Go On.mp3
+/home/paule/Music/Cuando.mp3
+/home/paule/Music/Despertar.mp3
+/home/paule/Music/El Camino.mp3
+/home/paule/Music/Felipe El Hombre.mp3
+/home/paule/Music/Hijos del Sol.mp3
+/home/paule/Music/Lo Mejor de Hoy.mp3
+/home/paule/Music/Mareo.mp3
+/home/paule/Music/Miedo.mp3
+/home/paule/Music/Otra Vez.mp3
+/home/paule/Music/Otro Dia.mp3
+/home/paule/Music/Otro Dia (Acustica).mp3
+/home/paule/Music/Ritual.mp3
+/home/paule/Music/Sin Frenos.mp3
+/home/paule/Music/Damage.mp3
+/home/paule/Music/Under And Over It.mp3
+/home/paule/Music/Handlebars.mp3
+/home/paule/Music/Run.mp3
+/home/paule/Music/Remember The Name.mp3
+/home/paule/Music/Whered You Go.mp3
+/home/paule/Music/Iris.mp3
+/home/paule/Music/Clint Eastwood.mp3
+/home/paule/Music/El Mañana.mp3
+/home/paule/Music/Tomorrow Comes Today.mp3
+/home/paule/Music/You Don't Own Me ft. G-Eazy.mp3
+/home/paule/Music/American Idiot.mp3
+/home/paule/Music/Bang Bang.mp3
+/home/paule/Music/Basket Case.mp3
+/home/paule/Music/Boulevard of Broken Dreams.mp3
+/home/paule/Music/Holiday.mp3
+/home/paule/Music/Jesus Of Suburbia.mp3
+/home/paule/Music/Still Breathing.mp3
+/home/paule/Music/Paradise City.mp3
+/home/paule/Music/Sweet Child O' Mine.mp3
+/home/paule/Music/Welcome To The Jungle.mp3
+/home/paule/Music/Stereo Hearts.mp3
+/home/paule/Music/The Fighter.mp3
+/home/paule/Music/Sign Of The Times.mp3
+/home/paule/Music/No Es Que No Te Quiera.mp3
+/home/paule/Music/My Name Is Human.mp3
+/home/paule/Music/Bullet.mp3
+/home/paule/Music/Glory.mp3
+/home/paule/Music/Undead.mp3
+/home/paule/Music/We Are.mp3
+/home/paule/Music/4 My 1.mp3
+/home/paule/Music/Fallen.mp3
+/home/paule/Music/La Flaca.mp3
+/home/paule/Music/Me Gusta Como Eres.mp3
+/home/paule/Music/No Te Duermas.mp3
+/home/paule/Music/G.O.M.D..mp3
+/home/paule/Music/She Knows.mp3
+/home/paule/Music/Wet Dreamz.mp3
+/home/paule/Music/Work Out.mp3
+/home/paule/Music/Verde Mas Alla.mp3
+/home/paule/Music/Pain.mp3
+/home/paule/Music/The Middle.mp3
+/home/paule/Music/Bad Reputation.mp3
+/home/paule/Music/Amar y Querer.mp3
+/home/paule/Music/Anda y Ve.mp3
+/home/paule/Music/La Nave del Olvido.mp3
+/home/paule/Music/Plural Siendo Singular.mp3
+/home/paule/Music/Fuego.mp3
+/home/paule/Music/Tu Calor.mp3
+/home/paule/Music/Company.mp3
+/home/paule/Music/Love Yourself.mp3
+/home/paule/Music/Swimming Pools (Drank).mp3
+/home/paule/Music/Man On The Moon.mp3
+/home/paule/Music/Pursuit Of Happiness ft. MGMT.mp3
+/home/paule/Music/Soundtrack To My Life.mp3
+/home/paule/Music/Hopeless Boy.mp3
+/home/paule/Music/Ignorance.mp3
+/home/paule/Music/It Aint Me.mp3
+/home/paule/Music/Brillas.mp3
+/home/paule/Music/Locos.mp3
+/home/paule/Music/Mar.mp3
+/home/paule/Music/Souvenir.mp3
+/home/paule/Music/Zombies.mp3
+/home/paule/Music/Bleed It Out.mp3
+/home/paule/Music/Breaking the Habit.mp3
+/home/paule/Music/Faint.mp3
+/home/paule/Music/In The End.mp3
+/home/paule/Music/Numb.mp3
+/home/paule/Music/All I Do.mp3
+/home/paule/Music/Ballin.mp3
+/home/paule/Music/Desesperado.mp3
+/home/paule/Music/Mind of a Stoner.mp3
+/home/paule/Music/Sail.mp3
+/home/paule/Music/Till I Die.mp3
+/home/paule/Music/Wild Boy.mp3
+/home/paule/Music/Kevin.mp3
+/home/paule/Music/Otherside (Hip-Hop).mp3
+/home/paule/Music/Wings.mp3
+/home/paule/Music/Best Day Ever.mp3
+/home/paule/Music/Missed Calls.mp3
+/home/paule/Music/My Favorite Part.mp3
+/home/paule/Music/Weekend.mp3
+/home/paule/Music/Una Lady Como Tu.mp3
+/home/paule/Music/Dear Mom.mp3
+/home/paule/Music/Cold ft. Future.mp3
+/home/paule/Music/Don't Wanna Know.mp3
+/home/paule/Music/Alone.mp3
+/home/paule/Music/Keep It Mello.mp3
+/home/paule/Music/Por Última Vez.mp3
+/home/paule/Music/Last One Standing.mp3
+/home/paule/Music/Look On Down From The Bridge.mp3
+/home/paule/Music/Aqui Estoy.mp3
+/home/paule/Music/Mis Ojos Un Espejo.mp3
+/home/paule/Music/Regálame La Libertad.mp3
+/home/paule/Music/Adios.mp3
+/home/paule/Music/Mis Defectos.mp3
+/home/paule/Music/The Deceived.mp3
+/home/paule/Music/The Reality.mp3
+/home/paule/Music/The Sinner.mp3
+/home/paule/Music/The Victim.mp3
+/home/paule/Music/Jolene.mp3
+/home/paule/Music/Malibu.mp3
+/home/paule/Music/Reggae Ball.mp3
+/home/paule/Music/Chingo de Cheve.mp3
+/home/paule/Music/Fine, Great.mp3
+/home/paule/Music/Pothole.mp3
+/home/paule/Music/Rock Bottom.mp3
+/home/paule/Music/The Thrash Particle.mp3
+/home/paule/Music/The Weekend.mp3
+/home/paule/Music/Your Graduation.mp3
+/home/paule/Music/Gimme Tha Power.mp3
+/home/paule/Music/Famous Last Words.mp3
+/home/paule/Music/Helena.mp3
+/home/paule/Music/I'm Not Okay (I Promise).mp3
+/home/paule/Music/Teenagers.mp3
+/home/paule/Music/The Ghost Of You.mp3
+/home/paule/Music/Welcome To The Black Parade.mp3
+/home/paule/Music/El Amor Acaba.mp3
+/home/paule/Music/Nunca Es Suficiente.mp3
+/home/paule/Music/A Part of Me.mp3
+/home/paule/Music/Close ft. Tove Lo.mp3
+/home/paule/Music/Make Me Cry.mp3
+/home/paule/Music/Fuck The Police.mp3
+/home/paule/Music/Wonderwall.mp3
+/home/paule/Music/Second and Sebring.mp3
+/home/paule/Music/Would You Still Be There.mp3
+/home/paule/Music/Cita en el quirofano.mp3
+/home/paule/Music/Disculpa los Malos Pensamientos.mp3
+/home/paule/Music/Hola!.mp3
+/home/paule/Music/Narcisista Por Excelencia.mp3
+/home/paule/Music/Nuestra Afliccion.mp3
+/home/paule/Music/Saludos Desde Turquía.mp3
+/home/paule/Music/Usted.mp3
+/home/paule/Music/Miss Jackson.mp3
+/home/paule/Music/This is Gospel.mp3
+/home/paule/Music/Perdono Y Olvido (MTV Unplugged).mp3
+/home/paule/Music/Prometiste.mp3
+/home/paule/Music/Another Brick In The Wall.mp3
+/home/paule/Music/Congratulations ft. Quavo.mp3
+/home/paule/Music/Another One Bites The Dust.mp3
+/home/paule/Music/Bohemian Rhapsody.mp3
+/home/paule/Music/Don't Stop Me Now.mp3
+/home/paule/Music/I Want To Break Free.mp3
+/home/paule/Music/We Will Rock You.mp3
+/home/paule/Music/Creep.mp3
+/home/paule/Music/Locked Away.mp3
+/home/paule/Music/By The Way.mp3
+/home/paule/Music/Californication.mp3
+/home/paule/Music/Can't Stop.mp3
+/home/paule/Music/Dani California.mp3
+/home/paule/Music/Dark Necesities.mp3
+/home/paule/Music/Otherside.mp3
+/home/paule/Music/Scar Tissue.mp3
+/home/paule/Music/The Adventures of Rain Dance Maggie.mp3
+/home/paule/Music/Under The Bridge.mp3
+/home/paule/Music/Face Down.mp3
+/home/paule/Music/Your Guardian Angel.mp3
+/home/paule/Music/Qué Gano Olvidándote.mp3
+/home/paule/Music/Give It All.mp3
+/home/paule/Music/Help Is On The Way.mp3
+/home/paule/Music/Hero Of War.mp3
+/home/paule/Music/Make It Stop.mp3
+/home/paule/Music/Prayer Of The Refugee.mp3
+/home/paule/Music/Savior.mp3
+/home/paule/Music/Swing Life Away.mp3
+/home/paule/Music/Shy Guys.mp3
+/home/paule/Music/Bad Liar.mp3
+/home/paule/Music/Good For You.mp3
+/home/paule/Music/Only You.mp3
+/home/paule/Music/The Heart Wants What It Wants.mp3
+/home/paule/Music/100 Frases.mp3
+/home/paule/Music/Dorian Gray.mp3
+/home/paule/Music/There's Nothing Holdin' Me Back.mp3
+/home/paule/Music/Treat You Better.mp3
+/home/paule/Music/Enemies.mp3
+/home/paule/Music/En Ésta No.mp3
+/home/paule/Music/Mientes Tan Bien.mp3
+/home/paule/Music/Suelta Mi Mano.mp3
+/home/paule/Music/Words.mp3
+/home/paule/Music/Better Off Dead.mp3
+/home/paule/Music/Kick Me.mp3
+/home/paule/Music/Killpop.mp3
+/home/paule/Music/Psychosocial.mp3
+/home/paule/Music/The Devil In I.mp3
+/home/paule/Music/De Musica Ligera.mp3
+/home/paule/Music/Santeria.mp3
+/home/paule/Music/Fake My Own Death .mp3
+/home/paule/Music/Fatlip.mp3
+/home/paule/Music/God Save Us All (Death to POP).mp3
+/home/paule/Music/In Too Deep.mp3
+/home/paule/Music/Pieces.mp3
+/home/paule/Music/Some Say.mp3
+/home/paule/Music/Still Waiting.mp3
+/home/paule/Music/The Hell Song.mp3
+/home/paule/Music/Underclass Hero.mp3
+/home/paule/Music/Walking Disaster.mp3
+/home/paule/Music/War.mp3
+/home/paule/Music/We're All To Blame.mp3
+/home/paule/Music/What We're All About.mp3
+/home/paule/Music/With Me.mp3
+/home/paule/Music/Am I A Psycho.mp3
+/home/paule/Music/Dysfunctional.mp3
+/home/paule/Music/Fragile.mp3
+/home/paule/Music/Robbers (Explicit).mp3
+/home/paule/Music/Dirty Little Secret.mp3
+/home/paule/Music/Move Along.mp3
+/home/paule/Music/Closer.mp3
+/home/paule/Music/Something Just Like This.mp3
+/home/paule/Music/Chicago.mp3
+/home/paule/Music/Danger Wildman.mp3
+/home/paule/Music/Holdfast.mp3
+/home/paule/Music/Mammoth.mp3
+/home/paule/Music/My Questions.mp3
+/home/paule/Music/R.I.T..mp3
+/home/paule/Music/Flashlight.mp3
+/home/paule/Music/Skeleton.mp3
+/home/paule/Music/Swimming Pool.mp3
+/home/paule/Music/Mr. Brightside.mp3
+/home/paule/Music/Somebody Told Me.mp3
+/home/paule/Music/When You Were Young.mp3
+/home/paule/Music/Always Where I Need To Be.mp3
+/home/paule/Music/Bad Habit.mp3
+/home/paule/Music/Naive.mp3
+/home/paule/Music/Self Esteem.mp3
+/home/paule/Music/The Kids Arent Alright.mp3
+/home/paule/Music/Youre Gonna Go Far Kid.mp3
+/home/paule/Music/Snow.mp3
+/home/paule/Music/Last Nite.mp3
+/home/paule/Music/Reptilia.mp3
+/home/paule/Music/Someday.mp3
+/home/paule/Music/Under Cover of Darkness.mp3
+/home/paule/Music/You Only Live Once.mp3
+/home/paule/Music/Bitter Sweet Symphony.mp3
+/home/paule/Music/Animal I Have Become.mp3
+/home/paule/Music/I Hate Everything About You.mp3
+/home/paule/Music/Stay High.mp3
+/home/paule/Music/Headstrong.mp3
+/home/paule/Music/Car Radio.mp3
+/home/paule/Music/Doubt.mp3
+/home/paule/Music/Fairly Local.mp3
+/home/paule/Music/Goner.mp3
+/home/paule/Music/Lane Boy.mp3
+/home/paule/Music/Sucker For Pain.mp3
+/home/paule/Music/Drown.mp3
+/home/paule/Music/IDGAF.mp3
+/home/paule/Music/Wounded Healer (Deer Tick Sample).mp3
+/home/paule/Music/Hope.mp3
+/home/paule/Music/Say It Ain't So.mp3
+/home/paule/Music/Teenage Dirtbag.mp3
+/home/paule/Music/Bad Influence.mp3
+/home/paule/Music/Paperbond.mp3
+/home/paule/Music/Promises.mp3
+/home/paule/Music/Maps.mp3
+/home/paule/Music/Desire.mp3
+/home/paule/Music/Ocean Avenue.mp3
+/home/paule/Music/10 A.M.mp3
+/home/paule/Music/Arrullo De Estrellas.mp3
+/home/paule/Music/Luna.mp3
+/home/paule/Music/Nada.mp3
+/home/paule/Music/Soñé.mp3
+/home/paule/Music/Azul Oscuro.mp3
